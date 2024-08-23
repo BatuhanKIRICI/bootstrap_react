eab5577540f36bf0e3750af5c4169f0cd97903ef
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-08-23 23:24:20 +0200

    "Typography examples"

--- src/App.js
+++ src/App.js
@@ -3,41 +3,43 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
-          </div>
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      <h1>Hello Bootsrap</h1>
+      <h2>Hello Bootsrap</h2>
+      <h3>Hello Bootsrap</h3>
+      <h4>Hello Bootsrap</h4>
+      <h5>Hello Bootsrap</h5>
+      <h6>Hello Bootsrap</h6>
+      <br />
+      <p className="fs-1">Hello Bootstrap</p>
+      <p className="h1">Hello Bootstrap</p>
+      <br />
+      <p>
+        You can use the mark tag to <mark>highlight</mark> text.
+      </p>
+      <p>
+        <del>This line of text is meant to be treated as deleted text.</del>
+      </p>
+      <p>
+        <s>This line of text is meant to be treated as no longer accurate.</s>
+      </p>
+      <p>
+        <ins>
+          This line of text is meant to be treated as an addition to the
+          document.
+        </ins>
+      </p>
+      <p>
+        <u>This line of text will render as underlined.</u>
+      </p>
+      <p>
+        <small>This line of text is meant to be treated as fine print.</small>
+      </p>
+      <p>
+        <strong>This line rendered as bold text.</strong>
+      </p>
+      <p>
+        <em>This line rendered as italicized text.</em>
+      </p>
     </div>
   );
 }
