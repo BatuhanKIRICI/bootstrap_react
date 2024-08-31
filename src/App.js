8c63e0f968ce29f8274b71eb24c1528557a4d58a
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-08-31 21:06:47 +0200

    "Text color and background color"

--- src/App.js
+++ src/App.js
@@ -3,43 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Bootsrap</h1>
-      <h2>Hello Bootsrap</h2>
-      <h3>Hello Bootsrap</h3>
-      <h4>Hello Bootsrap</h4>
-      <h5>Hello Bootsrap</h5>
-      <h6>Hello Bootsrap</h6>
-      <br />
-      <p className="fs-1">Hello Bootstrap</p>
-      <p className="h1">Hello Bootstrap</p>
-      <br />
-      <p>
-        You can use the mark tag to <mark>highlight</mark> text.
-      </p>
-      <p>
-        <del>This line of text is meant to be treated as deleted text.</del>
-      </p>
-      <p>
-        <s>This line of text is meant to be treated as no longer accurate.</s>
-      </p>
-      <p>
-        <ins>
-          This line of text is meant to be treated as an addition to the
-          document.
-        </ins>
-      </p>
-      <p>
-        <u>This line of text will render as underlined.</u>
-      </p>
-      <p>
-        <small>This line of text is meant to be treated as fine print.</small>
-      </p>
-      <p>
-        <strong>This line rendered as bold text.</strong>
-      </p>
-      <p>
-        <em>This line rendered as italicized text.</em>
-      </p>
+      <p className="text-danger bg-dark">Hello Bootstrap</p>
     </div>
   );
 }
