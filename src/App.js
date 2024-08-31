11200561b442dd69bcc6de3d6d5731187e8c3302
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-08-31 21:48:16 +0200

    "margin && padding"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p className="text-danger bg-dark">Hello Bootstrap</p>
+      <div className="text-warning bg-success mt-4 pt-5">division_1</div>
+      <div className="text-white bg-danger ms-2 p-2">division_2</div>
     </div>
   );
 }
